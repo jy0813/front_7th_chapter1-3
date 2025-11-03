524416479f5ed5d798de38f60f1f2c595ad1900b
jy0813 <busbug3@gmail.com>
2025-11-04 01:56:17 +0900

    fix: eslint 에러 수정

--- eslint.config.js
+++ eslint.config.js
@@ -72,7 +72,7 @@ export default [
       ...typescriptPlugin.configs.recommended.rules,
 
       // ESLint rules
-      'no-unused-vars': 'warn',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
 
       // React rules
       'react/prop-types': 'off',
